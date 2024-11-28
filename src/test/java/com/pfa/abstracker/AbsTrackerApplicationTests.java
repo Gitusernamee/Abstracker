07a18187c2d64682106ef729fdbd2ceb4a0ec3cd
mohammed <Mohammed.Sanbouli@emsi-edu.ma>
2024-11-28 19:19:51 +0100

    Project setup

---
+++ src/test/java/com/pfa/abstracker/AbsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfa.abstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

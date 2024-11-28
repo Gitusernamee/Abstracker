cfb41c019ec274bef404e98f483b4aa2cdfdecfa
mohammed <Mohammed.Sanbouli@emsi-edu.ma>
2024-11-28 21:19:19 +0100

    first implementation GlobalExceptionHandler class

---
+++ src/main/java/com/pfa/abstracker/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.pfa.abstracker.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleRuntimeException(RuntimeException ex) {
+        return ex.getMessage();
+    }
+}
